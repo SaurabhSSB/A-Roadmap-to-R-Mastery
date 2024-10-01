2df490cad25bf8c2f0dc4540df2b0ae6006c028b
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-02 01:47:23 +0530

    Update 17. Data Frame.R

--- 17. Data Frame.R
+++ 17. Data Frame.R
@@ -26,3 +26,37 @@ print(a1)
 
 a2<-student.data[2,]
 print(a2)
+a3<-student.data[2:4,]
+print(a3)
+a4<-student.data[2:3,2:4]
+print(a4)
+a5<-student.data[c(2:3),c(2:4)]
+print(a5)
+
+# Modification
+
+# Add Row
+
+a<-list(5,"Uryu",95,"2024-09-14")
+rbind(student.data,a)
+
+# Add Column
+
+b<-c("Shinigami-Hollow","Shinigami-Hollow",
+     "Shinigami-Hollow-Fullbring-Quincy","Fullbring")
+cbind(student.data,Specialisation = b)
+
+# Delete Row
+
+student.data<-student.data[-1,]
+print(student.data)
+
+# Delete Column
+
+student.data$first_day<-NULL
+print(student.data)
+
+# Summary : Extract Statistical Summary and Nature of Data
+
+print(summary(student.data))
+print(summary(student.data$roll_no.))
