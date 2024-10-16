cf7cda54e0842a7e1924e110ce67bf9741ea2e4a
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-10-17 00:28:54 +0530

    Updating Content

--- 22. Dplyr.R
+++ 22. Dplyr.R
@@ -1,13 +1,8 @@
 # Dplyr - Mainly used for data manipulation purpose in R language
-
 library(readxl)
-
 install.packages("dplyr")
-
 library("dplyr")
-
 data<-read_excel("C:/Users/Hp/Desktop/titanic.xlsx")
-
 # Verbs in Dplyr
 # select() - Select Columns
 # filter() - Filter rows
@@ -15,7 +10,6 @@ data<-read_excel("C:/Users/Hp/Desktop/titanic.xlsx")
 # mutate() - create new columns in dataframe
 # summarise() - summarise values
 # group_by() - group operation in split-apply-combine concept
-
 names(data)
 a_data <- select(data,name,age,embarked)
 head(a_data)
