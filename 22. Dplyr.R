a1b89833d3c70c1f8ab8e1fcb3829363b8cbb9d9
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-11-07 23:33:06 +0530

    Updating Content

--- 22. Dplyr.R
+++ 22. Dplyr.R
@@ -5,15 +5,10 @@ library(readxl)
 install.packages("dplyr")
 
 library("dplyr")
+
 data<-read_excel("C:/Users/Hp/Desktop/titanic.xlsx")
 
 # Verbs in Dplyr
-
-
-
-
-
-
 # select() - Select Columns
 # filter() - Filter rows
 # arrange() - re-order or arrange rows
