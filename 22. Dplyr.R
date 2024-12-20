b53003c1700437743e8fd37cf1dd44240e44596f
SaurabhSSB <saurabhsinghbhandarissb@gmail.com>
2024-12-20 21:18:28 +0530

    Updating Content

--- 22. Dplyr.R
+++ 22. Dplyr.R
@@ -1,4 +1,5 @@
 # Dplyr - Mainly used for data manipulation purpose in R language
+
 library(readxl)
 
 install.packages("dplyr")
@@ -9,8 +10,6 @@ data<-read_excel("C:/Users/Hp/Desktop/titanic.xlsx")
 
 # Verbs in Dplyr
 # select() - Select Columns
-
-
 # filter() - Filter rows
 # arrange() - re-order or arrange rows
 # mutate() - create new columns in dataframe
